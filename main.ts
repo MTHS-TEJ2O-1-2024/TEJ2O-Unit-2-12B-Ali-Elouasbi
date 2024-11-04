a929db871b28e6d0c14310b03939459bad951de7
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-11-04 11:17:43 -0500

    2-12

--- main.ts
+++ main.ts
@@ -5,4 +5,35 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// Variables
+let distanceToObject = 0
+let neopixelStrip: neopixel.Strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+// Setup
+basic.showIcon(IconNames.Happy)
+
+// When button A is pressed
+input.onButtonPressed(Button.A, function () {
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // Turns on all neopixels Red
+    if (distanceToObject < 10) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    } else {
+        //turn on all neopixel Green
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    }
+
+    // Show the colors on the strip
+    neopixelStrip.show()
+})
